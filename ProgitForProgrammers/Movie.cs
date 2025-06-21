3ad25f24b9a6528c45766fb4889bfc7395d79797
m-jeremic <jerremic@gmail.com>
2025-06-21 18:21:36 +0200

    Create Movie.cs
    Add Movie class

---
+++ ProgitForProgrammers/Movie.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ProGitForProgrammers
+{
+    public class Movie
+    {
+        public string Title { get; set; }
+        public List<string> Actors { get; set; }
+    }
+}
